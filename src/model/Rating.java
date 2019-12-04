8b7866eb0fb1f1507dc1c2f991883a2e264f41b5
Jesper Larsson <jespelar@kth.se>
2019-12-04 15:42:36 +0100

    Update Rating.java

--- src/model/Rating.java
+++ src/model/Rating.java
@@ -1,14 +1,15 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model;
+package booksdbclient.model;
 
 /**
  *
  * @author Johan C
  */
 public enum Rating {
-    ONE, TWO, THREE, FOUR, FIVE
+    NONE, ONE, TWO, THREE, FOUR, FIVE
 }
