657f1bad2104f6fb01531490bdc6dce64328de2e
Jesper Larsson <jespelar@kth.se>
2019-12-07 12:20:25 +0100

    Update BooksPane.java

--- src/view/BooksPane.java
+++ src/view/BooksPane.java
@@ -1,13 +1,18 @@
-package booksdbclient.view;
+package view;
 
-import booksdbclient.model.Author;
-import booksdbclient.model.SearchMode;
-import booksdbclient.model.Book;
-import booksdbclient.model.Genre;
-import booksdbclient.model.MockBooksDb;
-import booksdbclient.model.Rating;
+import java.io.IOException;
+import model.Author;
+import model.SearchMode;
+import model.Book;
+import model.Genre;
+import model.MockBooksDb;
+import model.Rating;
 import java.sql.Date;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -16,8 +21,11 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -27,6 +35,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.WindowEvent;
@@ -63,10 +72,10 @@ public class BooksPane extends VBox {
         booksInTable.clear();
         booksInTable.addAll(books);
     }
-    
+
     /**
      * Notify user on input error or exceptions.
-     * 
+     *
      * @param msg the message
      * @param type types: INFORMATION, WARNING et c.
      */
@@ -119,12 +128,10 @@ public class BooksPane extends VBox {
         titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
         isbnCol.setCellValueFactory(new PropertyValueFactory<>("isbn"));
         publishedCol.setCellValueFactory(new PropertyValueFactory<>("published"));
-        genreCol.setCellValueFactory(new PropertyValueFactory("genre"));        
+        genreCol.setCellValueFactory(new PropertyValueFactory("genre"));
         ratingCol.setCellValueFactory(new PropertyValueFactory("rating"));
         authorCol.setCellValueFactory(new PropertyValueFactory("authors"));
 
-
-        
         // associate the table view with the data
         booksTable.setItems(booksInTable);
     }
@@ -136,7 +143,7 @@ public class BooksPane extends VBox {
         searchModeBox.getItems().addAll(SearchMode.values());
         searchModeBox.setValue(SearchMode.Title);
         searchButton = new Button("Search");
-        
+
         // event handling (dispatch to controller)
         searchButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -160,42 +167,102 @@ public class BooksPane extends VBox {
         MenuItem titleItem = new MenuItem("Title");
         MenuItem isbnItem = new MenuItem("ISBN");
         MenuItem authorItem = new MenuItem("Author");
-        searchMenu.getItems().addAll(titleItem, isbnItem, authorItem);
+        MenuItem genreItem = new MenuItem("Genre");
+        MenuItem allItems = new MenuItem("Get all books");
+        searchMenu.getItems().addAll(titleItem, isbnItem, authorItem, genreItem, allItems);
 
         Menu manageMenu = new Menu("Manage");
         MenuItem addItem = new MenuItem("Add");
+
         MenuItem removeItem = new MenuItem("Remove");
         MenuItem updateItem = new MenuItem("Update");
         manageMenu.getItems().addAll(addItem, removeItem, updateItem);
-        
-        connectItem.setOnAction(new EventHandler<ActionEvent>(){
+
+        connectItem.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                controller.connect();   
+                controller.connect();
             }
         });
-        
-        exitItem.setOnAction(new EventHandler<ActionEvent>(){
-          @Override
-          public void handle(ActionEvent event){
-              event.consume();
-              Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you want to close application?");
-              alert.showAndWait();
-              if(alert.getResult() == ButtonType.OK){
-                  controller.disconnect(); 
-                  Platform.exit();
-              }
-          }
-        });
-        
-        disconnectItem.setOnAction(new EventHandler<ActionEvent>(){
+
+        exitItem.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                controller.disconnect();    
+                event.consume();
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you want to close application?");
+                alert.showAndWait();
+                if (alert.getResult() == ButtonType.OK) {
+                    controller.disconnect();
+                    Platform.exit();
+                }
             }
+        });
+
+        disconnectItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                controller.disconnect();
+            }
+        });
+
+        addItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    addBookInit(controller);
+                } catch (IOException ex) {
+                    Logger.getLogger(BooksPane.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (SQLException ex) {
+                    Logger.getLogger(BooksPane.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+
+        allItems.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                controller.getAllBooks();
+            }
+
         });
 
         menuBar = new MenuBar();
         menuBar.getMenus().addAll(fileMenu, searchMenu, manageMenu);
     }
+    
+            
+        private void addBookInit(Controller controller) throws IOException, SQLException{
+                Dialog dialog = new Dialog();
+                ButtonType button = new ButtonType("Create", ButtonData.OK_DONE);
+		dialog.getDialogPane().getButtonTypes().add(button);
+		dialog.setTitle("Add book");
+                GridPane addBookPane = new GridPane();
+                ArrayList<TextField> contextTextField = new ArrayList<>();
+                addBookTable(dialog, addBookPane, contextTextField);
+                dialog.getDialogPane().setContent(addBookPane);  
+                dialog.showAndWait();
+		dialog.setResizable(true);
+                if(button.getButtonData()== ButtonData.OK_DONE){
+                    String isbn = contextTextField.get(0).getText();
+                    String title = contextTextField.get(1).getText();
+                    String genre = contextTextField.get(2).getText();
+                    String date = contextTextField.get(3).getText();
+                    controller.addBook(isbn, title, genre, date);
+                }
+        }
+        
+        private void addBookTable(Dialog dialog, GridPane addBookPane, ArrayList<TextField> contextTextField){
+                ArrayList<Label> context = new ArrayList<>();
+                context.add(new Label("ISBN"));
+                context.add(new Label("Title"));
+                //context.add(new Label("Author"));
+                context.add(new Label("Genre"));
+                context.add(new Label("Date"));
+                             
+                for(int i = 0; i < context.size(); i++){
+                    contextTextField.add(new TextField());
+                    addBookPane.add(context.get(i), 0, i);
+                    addBookPane.add(contextTextField.get(i), 1, i);
+                }           
+        }
 }
